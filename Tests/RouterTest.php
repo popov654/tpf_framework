be3f42a8a604a5698ec6ad0ca8479777f92bf9b0
popov654 <popov654@yandex.ru>
2025-01-01 03:35:11 +0300

    Added tests for new endpoints

--- Tests/RouterTest.php
+++ Tests/RouterTest.php
@@ -3,7 +3,10 @@
 namespace Tpf\Tests;
 
 use Symfony\Component\HttpFoundation\Request;
+use Tpf\Database\Repository;
+use Tpf\Model\Session;
 use Tpf\Model\User;
+use Tpf\Service\Auth\Auth;
 use Tpf\Service\Router\Router;
 
 class RouterTest extends BasicTest
@@ -44,12 +47,99 @@ class RouterTest extends BasicTest
         global $TPF_REQUEST;
         $TPF_REQUEST = [];
 
+        dbConnect();
+
         $request = Request::create('/getEntity?type=user&id=1');
         $response = Router::route($request);
         self::assertEquals('application/json', $response->headers->get('Content-Type'));
-        $user = json_decode($response->getContent());
-        self::assertEquals(1, $user->id);
-        self::assertEquals('admin', $user->username);
-        self::assertEquals(User::ROLE_ADMIN, $user->role);
+
+        /* Should give an error when we are not authorized */
+        self::assertGreaterThanOrEqual(400, $response->getStatusCode());
+
+        $accessToken = Utils::getAuthToken('admin', 'password');
+
+        if ($accessToken != null) {
+
+            $request = Request::create('/getEntity?type=user&id=1');
+            $request->headers->add(['Authorization' => 'Bearer ' . $accessToken]);
+            $TPF_REQUEST['session'] = Auth::authenticate($request);
+            $response = Router::route($request);
+
+            self::assertEquals(200, $response->getStatusCode());
+            self::assertEquals('application/json', $response->headers->get('Content-Type'));
+
+            $user = json_decode($response->getContent());
+            self::assertEquals(1, $user->id);
+            self::assertEquals('admin', $user->username);
+            self::assertEquals(User::ROLE_ADMIN, $user->role);
+
+            Utils::endSession(1, $accessToken);
+        }
+    }
+
+    public function testGetPosts()
+    {
+        global $TPF_REQUEST;
+        $TPF_REQUEST = [];
+
+        dbConnect();
+
+        require_once PATH . '/src/Model/Entity.php';
+        require_once PATH . '/src/Model/Blog/Post.php';
+        $repository = new Repository('App\\Model\\Blog\\Post');
+        $posts = $repository->fetch();
+
+        if (count($posts) == 0) {
+            Utils::seedBlogPosts();
+        }
+
+        $request = Request::create('/getEntity?type=blog_post&id=1');
+        $response = Router::route($request);
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertEquals('application/json', $response->headers->get('Content-Type'));
+
+        $post = json_decode($response->getContent());
+        self::assertEquals(1, $post->id);
+        self::assertEquals(1, $post->author_id);
+        self::assertEquals('First blog entry', $post->name);
+        self::assertFalse($post->isDeleted);
+
+        $request = Request::create('/getEntities?type=blog_post');
+        $response = Router::route($request);
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertEquals('application/json', $response->headers->get('Content-Type'));
+
+        $posts = json_decode($response->getContent());
+
+        self::assertGreaterThanOrEqual(2, count($posts));
+        self::assertGreaterThanOrEqual(2, $posts[0]->id);
+    }
+
+    public function testGetPostSchema()
+    {
+        global $TPF_REQUEST;
+        $TPF_REQUEST = [];
+
+        dbConnect();
+
+        $accessToken = Utils::getAuthToken('admin', 'password');
+
+        $request = Request::create('/getSchema?type=blog_post');
+        $request->headers->add(['Authorization' => 'Bearer ' . $accessToken]);
+        $TPF_REQUEST['session'] = Auth::authenticate($request);
+        $response = Router::route($request);
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertEquals('application/json', $response->headers->get('Content-Type'));
+
+        $schema = json_decode($response->getContent(), true);
+        self::assertTrue(isset($schema['id']));
+        self::assertEquals('text', $schema['name']);
+        self::assertEquals('text', $schema['text']);
+        self::assertEquals('bool', $schema['isActive']);
+        self::assertEquals('bool', $schema['isDeleted']);
+
+        Utils::endSession(1, $accessToken);
     }
 }
--- Tests/Utils.php
+++ Tests/Utils.php
@@ -3,6 +3,7 @@
 namespace Tpf\Tests;
 
 use Symfony\Component\HttpFoundation\Request;
+use Tpf\Database\AbstractEntity;
 use Tpf\Database\Repository;
 use Tpf\Model\Session;
 use Tpf\Model\User;
@@ -25,13 +26,47 @@ class Utils
 
     public static function endSession(int $userId, string $token)
     {
-        global $TPF_CONFIG, $TPF_REQUEST;
+        global $TPF_CONFIG, $TPF_REQUEST, $dbal;
         $session = (new Repository(Session::class))->findOneBy(['user_id' => $userId, 'secure_session_id' => $token]);
         if ($session) {
             $session->delete();
-            /** @var PDO $dbal */
+            /** @var \PDO $dbal */
             $dbal->exec('ALTER TABLE `'. $TPF_CONFIG['db']['database'] .'`.`session` AUTO_INCREMENT=0;');
             unset($TPF_REQUEST['session']);
+        }
+    }
+
+    public static function seedBlogPosts()
+    {
+        $time = (new \DateTime())->format('Y-m-d H:i:s');
+        $posts = [
+            [
+                'name' => 'First blog entry',
+                'text' => 'Content 1',
+                'image' => 'website-3483020_640.png',
+                'author_id' => 1,
+                'is_active' => 1,
+                'is_deleted' => 0,
+                'created_at' => $time,
+                'modified_at' => $time
+            ],
+            [
+                'name' => 'Second blog entry',
+                'text' => 'Content 2',
+                'image' => 'website-3374825_1920.jpg',
+                'author_id' => 1,
+                'is_active' => 1,
+                'is_deleted' => 0,
+                'created_at' => $time,
+                'modified_at' => $time
+            ]
+        ];
+        require_once PATH . '/src/Model/Entity.php';
+        require_once PATH . '/src/Model/Blog/Post.php';
+        foreach ($posts as $postData) {
+            $post = new App\Model\Blog\Post();
+            AbstractEntity::fillFromArray($post, $postData);
+            $post->save();
         }
     }
 }
