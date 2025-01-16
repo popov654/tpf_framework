c17f8f02e89828a53fdc0cc0f8f412d135cd854e
popov654 <popov654@yandex.ru>
2025-01-16 03:33:47 +0300

    Moved BasicEntity class to system space

---
+++ Model/BasicEntity.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tpf\Model;
+
+
+abstract class BasicEntity extends AbstractEntity
+{
+    public int $id;
+    public string $name;
+    public string $text;
+    public string $image;
+    public array $tags = [];
+    public int $authorId;
+    public User $author;
+    public bool $isActive;
+    public bool $isDeleted;
+    public \Datetime $createdAt;
+    public \Datetime $modifiedAt;
+
+
+    public function __construct()
+    {
+        global $TPF_REQUEST;
+        $now = new \Datetime();
+        parent::__construct();
+        if ($TPF_REQUEST['session']) {
+            $this->authorId = $TPF_REQUEST['session']->user->id;
+        }
+        $this->createdAt = $now;
+        $this->modifiedAt = $now;
+    }
+
+}
