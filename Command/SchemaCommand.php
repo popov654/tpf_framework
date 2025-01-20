7c7f68eabf68f7db219ac7c645fa289363a06f0e
popov654 <popov654@yandex.ru>
2025-01-21 02:41:12 +0300

    Implemented `schema:diff` command

--- Command/SchemaCommand.php
+++ Command/SchemaCommand.php
@@ -30,4 +30,36 @@ class SchemaCommand
             print "Schema is up to date.";
         }
     }
+
+    public static function diff(array $args)
+    {
+        $diffs = getEntitySchemaDiff($args[0] ?? '*');
+
+        print "Calculating diff..." . "\r\n";
+
+        foreach ($diffs as $className => $diff) {
+            if (empty($diff)) continue;
+
+            $existingColumns = getEntityTableColumns($className);
+
+            $del = []; $add = [];
+
+            foreach ($diff as $entry) {
+                for ($i = 0; $i < $entry['deleteCount']; $i++) {
+                    $el = $existingColumns[$entry['position'] + $i];
+                    $del[] = strtoupper($el['Type']) . ' ' . $el['Field'];
+                }
+                foreach ($entry['add'] as $field) {
+                    $add[] = implode(' ', array_reverse(array_slice(explode(' ', $field['full']), 0, 2)));
+                }
+            }
+
+            $str = trim((!empty($del) ? '- ' . implode(', ', $del) : '') . '; ' . (!empty($add) ? '+ ' . implode(', ', $add) : ''), '; ');
+
+            if (!empty($str) > 0) {
+                print "\r\n" . $className . ":\r\n";
+                print $str . "\r\n";
+            }
+        }
+    }
 }
