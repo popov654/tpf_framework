e79f857b43907365b20ab87e049a00492553a00b
popov654 <popov654@yandex.ru>
2025-01-22 21:15:51 +0300

    Small fixes

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,5 +1,58 @@
 function initSearch() {
+	
+	document.querySelectorAll('[data-action="toggle-search"]').forEach(el => {
+		el.addEventListener('click', function(event) {
+			this.parentNode.parentNode.classList.toggle('active')
+			if (this.parentNode.parentNode.classList.contains('active')) {
+				this.parentNode.getElementsByTagName('input')[0].focus()
+			}
+		});
+	});
+	
+	document.querySelectorAll('[data-action="clear-search"]').forEach(el => {
+		el.addEventListener('click', function(event) {
+			clearSearchSuggestions()
+			document.querySelector('.search_wrap .dropdown').style.display = 'none'
+			if (this.parentNode.getElementsByTagName('input')[0].value.length && window.isSearch) {
+				this.parentNode.getElementsByTagName('input')[0].value = ''
+				reloadContent(true, true)
+				toggleSearchOptions(true)
+			}
+			this.parentNode.getElementsByTagName('input')[0].value = ''
+			this.parentNode.parentNode.classList.remove('active')
+		});
+	});
+	
 	document.querySelectorAll('.search-input-field').forEach(input => {
+		input.addEventListener('keydown', debounce(function(event) {
+			if (event.key == 'Enter' || event.key == 'Tab') {
+				let line = document.querySelector('#search_variants .line.selected')
+				if (line) line.click()
+			}
+		}, 500))
+		input.addEventListener('keydown', function(event) {
+			let line = document.querySelector('#search_variants .line.selected')
+			let container = document.querySelector('#search_variants')
+			if (container.configured) container = container.children[0]
+			
+			if (event.key.match(/^Arrow(Up|Down)$/)) {
+				event.preventDefault()
+				let pos = line ? Array.prototype.indexOf.call(container.children, line) : 0
+				
+				if (line && event.key == 'ArrowUp') {
+					pos--
+					if (pos < 0) pos = container.children.length - 1
+				}
+				if (line && event.key == 'ArrowDown') {
+					pos++
+					if (pos >= container.children.length) pos = 0
+				}
+				
+				Array.from(container.children).forEach((line, index) => {
+					line.classList.toggle('selected', index == pos)
+				})
+			}
+		})
 		input.addEventListener('input', debounce(function(event) {
 			if (event.target.value.length > 1) {
 				// Search by item ID
@@ -32,12 +85,12 @@ function initSearch() {
 				suggestItemIds(event.target)
 				return
 			}
-			if (event.target.value.slice(0, 1) == '@') {
-				toggleSearchOptions(false)
-				suggestAuthorUsernames(event.target)
-				event.target.oldValue = event.target.value
-				return
-			}
+			// if (event.target.value.slice(0, 1) == '@') {
+				// toggleSearchOptions(false)
+				// suggestAuthorUsernames(event.target)
+				// event.target.oldValue = event.target.value
+				// return
+			// }
 		});
 		input.addEventListener('blur', function(event) {
 			if (event.target.value.length > 1) {
@@ -50,22 +103,20 @@ function initSearch() {
 				}
 				// Search by author username
 				if (event.target.value.slice(0, 1) == '@') {
-					window.searchTimer = setTimeout(() => {
-						clearSearchSuggestions()
-						document.querySelector('.search_wrap .dropdown').style.display = 'none'
-						searchByAuthorUsername(event.target.value)
-					}, 800)
 					return
 				}
-			} else {
-				clearSearchSuggestions()
-				document.querySelector('.search_wrap .dropdown').style.display = 'none'
-				reloadContent(true, true)
-				toggleSearchOptions(true)
 			}
 		});
 		document.querySelectorAll('.search_wrap .options input[type="checkbox"]').forEach(input => {
 			input.addEventListener('change', () => reloadContent(true, true))
+		})
+	});
+	
+	document.getElementById('search_variants').addEventListener('mouseover', function(event) {
+		if (!event.target.classList.contains('line')) return
+		let lines = event.target.parentNode.children
+		Array.from(lines).forEach(function(line) {
+			line.classList.toggle('selected', line == event.target)
 		})
 	});
 	
@@ -105,6 +156,9 @@ function initSearch() {
 					let line = createItemLine(item)
 					list.appendChild(line)
 				}
+				if (list.children.length) {
+					list.children[0].classList.add('selected')
+				}
 				if (!list.clientHeight) document.querySelector('.search_wrap .dropdown').style.visibility = 'hidden'
 				setTimeout(function() {
 					list.parentNode.style.height = list.clientHeight + 'px'
@@ -133,6 +187,7 @@ function initSearch() {
 		let container = document.querySelector('#page-content .items-list');
 		container.innerHTML = '';
 		updatePagination(1);
+		window.isSearch = true;
 		container.appendChild(line);
 		document.querySelector('.main .form').style.visibility = '';
 		line.click();
@@ -161,6 +216,9 @@ function initSearch() {
 					if (fullName.length) line.lastElementChild.lastElementChild.textContent = '(' + fullName + ')'
 					list.appendChild(line)
 				}
+				if (list.children.length) {
+					list.children[0].classList.add('selected')
+				}
 				if (!list.clientHeight) document.querySelector('.search_wrap .dropdown').style.visibility = 'hidden'
 				setTimeout(function() {
 					list.parentNode.style.height = list.clientHeight + 'px'
@@ -184,4 +242,6 @@ function initSearch() {
 		
 		reloadContent(true, true)
 	}
+	
+	window.isSearch = false
 }
